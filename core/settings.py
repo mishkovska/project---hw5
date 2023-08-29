303c04b5d02b7473f7f91ced7c6137a693bc6141
mishkovska <103728024+mishkovska@users.noreply.github.com>
2023-08-29 14:49:29 +0200

    email production ready

--- core/settings.py
+++ core/settings.py
@@ -1,5 +1,8 @@
 from pathlib import Path
+from dotenv import load_dotenv
 import os
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -9,10 +12,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-d5nc9jzfmz5g0n9j#9q1f^ig6#qpcsdu5rs_55&mc384t4xowc'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG') == 'True'
 
 ALLOWED_HOSTS = ['*']
 
@@ -135,10 +138,16 @@ LOGIN_REDIRECT_URL = '/account/dashboard'
 LOGIN_URL = '/account/login/'
 
 # Email setting
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
+EMAIL_USE_TLS = True
+# EMAIL_USE_SSL = True
 
 # Stripe Payment
-PUBLISHABLE_KEY = 'pk_test_51NdwzECloRCyzDNGjMMn4YcCPrui42DNxUjsgbCcdefWm66ygk40YUcr5Wat8Gu9aOFmcmUqrsthqvX2OKEdRyg9009tSaGcJl'
-SECRET_KEY = 'sk_test_51NdwzECloRCyzDNGuvBbXBqPjibk9RpQ4ezYdqRlQeudnuyRSQnnv4ujcM1ewlJtbZ6iJEUXjtAqDvrfs3qsAbRK00nuWKnuYb'
-STRIPE_ENDPOINT_SECRET = 'whsec_3b5af4c54d314b2b0370316a6f97dd1e80706ca46a23884781c6d41eeb564da3'
+PUBLISHABLE_KEY = os.environ.get('PUBLISHABLE_KEY')
+SECRET_KEY = os.environ.get('SECRET_KEY')
+STRIPE_ENDPOINT_SECRET = os.environ.get('STRIPE_ENDPOINT_SECRET')
 # stripe listen --forward-to localhost:8000/payment/webhook/
